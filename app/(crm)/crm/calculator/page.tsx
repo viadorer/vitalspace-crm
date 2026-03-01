410c04dfb2472d489b1904957d36cf8243cd9f43
David Choc <viadorer@gmail.com>
2026-03-01 16:15:57 +0100

    fix: remove conflicting export directives from calculator page

--- app/(crm)/crm/calculator/page.tsx
+++ app/(crm)/crm/calculator/page.tsx
@@ -1,7 +1,5 @@
 'use client'
 
-export const dynamic = 'force-dynamic'
-
 import { useState } from 'react'
 import { QuoteCalculator } from '@/components/crm/QuoteCalculator'
 import { SaveQuoteModal } from '@/components/crm/SaveQuoteModal'
