dc2b12d7fe2f0dc4dfa5f9a560d904a208a87bb6
David Choc <viadorer@gmail.com>
2026-03-04 22:54:32 +0100

    fix: remove webpack config causing Turbopack build error

--- next.config.js
+++ next.config.js
@@ -3,20 +3,6 @@ const nextConfig = {
   images: {
     domains: ['localhost'],
   },
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.resolve.alias = {
-        ...config.resolve.alias,
-        'pdfkit': require.resolve('pdfkit'),
-      }
-    }
-    return config
-  },
-  outputFileTracingIncludes: {
-    '/api/quotes/[id]/pdf': [
-      './node_modules/pdfkit/js/data/**/*',
-    ],
-  },
 }
 
 module.exports = nextConfig
