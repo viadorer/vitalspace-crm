a6c7ec74c9364adfea033180d3237b1b3bba0196
David Choc <viadorer@gmail.com>
2026-03-04 22:47:02 +0100

    fix: include PDFKit data files in Vercel build via outputFileTracingIncludes

--- next.config.js
+++ next.config.js
@@ -3,6 +3,20 @@ const nextConfig = {
   images: {
     domains: ['localhost'],
   },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        'pdfkit': require.resolve('pdfkit'),
+      }
+    }
+    return config
+  },
+  outputFileTracingIncludes: {
+    '/api/quotes/[id]/pdf': [
+      './node_modules/pdfkit/js/data/**/*',
+    ],
+  },
 }
 
 module.exports = nextConfig
